0107c6a0a214e5b2bf841df7e8ce19d17a406778
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-01 23:52:18 -0800

    commit tree 0e2152eca0b91717c3ab7193a1359c5a1cdaa9f7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(obj, cb) {
+    const arr = []
+    for (const key in obj) {
+      arr.push(cb(obj[key]))
+    }
+    return arr
+  }
+
+  function reduce(obj, cb, accumulator) {
+    const keys = Object.keys(obj)
+    let i = 1
+  
+    if (accumulator) {
+      i = 0
+    } else {
+      accumulator = obj[keys[0]]
+    }
+  
+    for (i; i < keys.length; i++) {
+      accumulator = cb(accumulator, obj[keys[i]])
+    }
+    return accumulator
+  }
